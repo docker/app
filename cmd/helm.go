280926ae77119c92ed3694fc4d6535bea61ca9dc
Matthieu Nottale <matthieu.nottale@docker.com>
2018-04-11 16:05:38 +0200

    Helm renderer.
    Signed-off-by: Matthieu Nottale <matthieu.nottale@docker.com>

---
+++ cmd/helm.go
@@ -0,0 +1,43 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/docker/lunchbox/packager"
+	"github.com/spf13/cobra"
+)
+
+var helmCmd = &cobra.Command{
+	Use:   "helm <app-name> [-c <compose-files>...] [-e key=value...] [-f settings-file...]",
+	Short: "Render the composefile for this app as an Helm package",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		d := make(map[string]string)
+		for _, v := range helmEnv {
+			kv := strings.SplitN(v, "=", 2)
+			if len(kv) != 2 {
+				fmt.Printf("Malformed env input: '%s'\n", v)
+				os.Exit(1)
+			}
+			d[kv[0]] = kv[1]
+		}
+		err := packager.Helm(args[0], helmComposeFiles, helmSettingsFile, d)
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
+	},
+}
+
+var helmComposeFiles []string
+var helmSettingsFile []string
+var helmEnv []string
+
+func init() {
+	rootCmd.AddCommand(helmCmd)
+	helmCmd.Flags().StringArrayVarP(&helmComposeFiles, "compose-files", "c", []string{}, "Override Compose files")
+	helmCmd.Flags().StringArrayVarP(&helmSettingsFile, "settings-files", "s", []string{}, "Override settings files")
+	helmCmd.Flags().StringArrayVarP(&helmEnv, "env", "e", []string{}, "Override environment values")
+}
---
+++ packager/helm.go
@@ -0,0 +1,91 @@
+package packager
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+
+	conversion "github.com/docker/cli/cli/command/stack/kubernetes"
+	"github.com/docker/cli/kubernetes/compose/v1beta2"
+	"github.com/docker/lunchbox/types"
+	yaml "gopkg.in/yaml.v2"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+type helmMaintainer struct {
+	Name string
+}
+
+type helmMeta struct {
+	Name        string
+	Version     string
+	Description string
+	Keywords    []string
+	Maintainers []helmMaintainer
+}
+
+func toHelmMeta(meta *types.AppMetadata) (*helmMeta, error) {
+	return &helmMeta{
+		Name:        meta.Application.Name,
+		Version:     meta.Version,
+		Description: meta.Application.Description,
+		Keywords:    meta.Application.Labels,
+		Maintainers: []helmMaintainer{{Name: meta.Application.Author}},
+	}, nil
+}
+
+// Helm renders an app as an Helm Chart
+func Helm(appname string, composeFiles []string, settingsFile []string, env map[string]string) error {
+	appname, cleanup, err := Extract(appname)
+	if err != nil {
+		return err
+	}
+	defer cleanup()
+	rendered, err := Render(appname, composeFiles, settingsFile, env)
+	if err != nil {
+		return err
+	}
+	metaFile := path.Join(appname, "metadata.yml")
+	metaContent, err := ioutil.ReadFile(metaFile)
+	if err != nil {
+		return err
+	}
+	var meta types.AppMetadata
+	err = yaml.Unmarshal(metaContent, &meta)
+	if err != nil {
+		return err
+	}
+	targetDir := appName(appname) + ".helm"
+	os.Mkdir(targetDir, 0755)
+	hmeta, err := toHelmMeta(&meta)
+	if err != nil {
+		return err
+	}
+	hmetadata, err := yaml.Marshal(hmeta)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path.Join(targetDir, "Chart.yaml"), hmetadata, 0644)
+	if err != nil {
+		return err
+	}
+	os.Mkdir(path.Join(targetDir, "templates"), 0755)
+	stackSpec := conversion.FromComposeConfig(rendered)
+	stack := v1beta2.Stack{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "stacks.compose.docker.com",
+			APIVersion: "v1beta2",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      appName(appname),
+			Namespace: "default", // FIXME
+		},
+		Spec: stackSpec,
+	}
+	stackData, err := yaml.Marshal(stack)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path.Join(targetDir, "templates", "stack.yaml"), stackData, 0644)
+	return err
+}
