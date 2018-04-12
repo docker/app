48f17c09375cb3820d9bce2b75c9ac342ad5a920
Matthieu Nottale <matthieu.nottale@docker.com>
2018-04-12 11:16:18 +0200

    inspect: Dump metadatas.
    Signed-off-by: Matthieu Nottale <matthieu.nottale@docker.com>

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/docker/lunchbox/packager"
 	"github.com/spf13/cobra"
 )
 
@@ -10,21 +12,16 @@ import (
 var inspectCmd = &cobra.Command{
 	Use:   "inspect <app-name>",
 	Short: "Retrieve metadata for a given app package",
+	Args:  cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("inspect called")
+		err := packager.Inspect(args[0])
+		if err != nil {
+			fmt.Printf("%v\n", err)
+			os.Exit(1)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(inspectCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// inspectCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// inspectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
