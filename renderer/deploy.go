c72e4a18ab8c1d2429f95fa6dbd6db50c41ae6a5
Matthieu Nottale <matthieu.nottale@docker.com>
2018-05-04 17:11:41 +0200

    deploy: Implement.
    Signed-off-by: Matthieu Nottale <matthieu.nottale@docker.com>

---
+++ renderer/deploy.go
@@ -0,0 +1,40 @@
+package renderer
+
+import (
+	"context"
+	"os"
+
+	"github.com/docker/cli/cli/command"
+	"github.com/docker/cli/cli/command/stack/kubernetes"
+	"github.com/docker/cli/cli/command/stack/options"
+	"github.com/docker/cli/cli/command/stack/swarm"
+	cliflags "github.com/docker/cli/cli/flags"
+)
+
+// Deploy deploys this app, merging in settings files, other compose files, end env
+func Deploy(appname string, composeFiles []string, settingsFile []string, env map[string]string,
+	orchestrator string, kubeconfig string, namespace string) error {
+	rendered, err := Render(appname, composeFiles, settingsFile, env)
+	if err != nil {
+		return err
+	}
+	cli := command.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, true)
+	cli.Initialize(&cliflags.ClientOptions{
+		Common: &cliflags.CommonOptions{
+			Orchestrator: orchestrator,
+		},
+	})
+	if orchestrator == "swarm" {
+		ctx := context.Background()
+		return swarm.DeployCompose(ctx, cli, rendered, options.Deploy{})
+	}
+	// kube mode
+	kubeCli, err := kubernetes.WrapCli(cli, kubernetes.Options{
+		Namespace: namespace,
+		Config:    kubeconfig,
+	})
+	if err != nil {
+		return err
+	}
+	return kubernetes.DeployStack(kubeCli, options.Deploy{}, rendered)
+}
--- vendor/github.com/docker/cli/cli/command/stack/kubernetes/deploy.go
+++ vendor/github.com/docker/cli/cli/command/stack/kubernetes/deploy.go
@@ -5,17 +5,27 @@ import (
 
 	"github.com/docker/cli/cli/command/stack/loader"
 	"github.com/docker/cli/cli/command/stack/options"
+	composetypes "github.com/docker/cli/cli/compose/types"
 	"github.com/pkg/errors"
 )
 
 // RunDeploy is the kubernetes implementation of docker stack deploy
 func RunDeploy(dockerCli *KubeCli, opts options.Deploy) error {
-	cmdOut := dockerCli.Out()
 	// Check arguments
 	if len(opts.Composefiles) == 0 {
 		return errors.Errorf("Please specify only one compose file (with --compose-file).")
 	}
 
+	// Parse the compose file
+	cfg, err := loader.LoadComposefile(dockerCli, opts)
+	if err != nil {
+		return err
+	}
+	return DeployStack(dockerCli, opts, cfg)
+}
+
+func DeployStack(dockerCli *KubeCli, opts options.Deploy, cfg *composetypes.Config) error {
+	cmdOut := dockerCli.Out()
 	// Initialize clients
 	stacks, err := dockerCli.stacks()
 	if err != nil {
@@ -26,11 +36,6 @@ func RunDeploy(dockerCli *KubeCli, opts options.Deploy) error {
 		return err
 	}
 
-	// Parse the compose file
-	cfg, err := loader.LoadComposefile(dockerCli, opts)
-	if err != nil {
-		return err
-	}
 	stack, err := stacks.FromCompose(opts.Namespace, *cfg)
 	if err != nil {
 		return err
