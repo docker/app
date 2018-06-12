1d2b859c46374ae4bef4741ac5bfbcd5f033d435
Antony MECHIN <antony.mechin@docker.com>
2018-06-12 10:36:25 +0200

    Add constant ImageLabel.
    Signed-off-by: Antony MECHIN <antony.mechin@docker.com>

--- internal/image/image.go
+++ internal/image/image.go
@@ -64,7 +64,7 @@ func Load(appname string, services []string) error {
 	return nil
 }
 
-// List images with the label 'com.docker.application'.
+// List images with the label specific to applications.
 func List(appname string) error {
 	cmd := exec.Command("docker", "image", "ls", "--format='{{.Repository}}:{{.Tag}}'", "--filter", "label=com.docker.application")
 	output, err := cmd.CombinedOutput()
