c88d97a9b7cf398b4320635bf5d84fae6b79ab6c
Silvin Lubecki <silvin.lubecki@docker.com>
2018-07-24 15:42:34 +0200

    Clean unused constants
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- internal/config.go
+++ internal/config.go
@@ -1,10 +1,6 @@
 package internal
 
 var (
-	// Debug enables debug logging and features
-	Debug = false
 	// Experimental enables experimental features if set to "on"
 	Experimental = "on"
-	// Renderers is a comma-separated list of enabled renderers
-	Renderers = "gotemplate"
 )
