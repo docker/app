bb9310fc27a9668dbfd476daed865117e4d0605c
Silvin Lubecki <silvin.lubecki@docker.com>
2018-09-04 16:32:31 +0200

    Add compose file summary (services, networks, volumes and secrets) to the inspect command * Refactor inspect command
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- internal/inspect/inspect.go
+++ internal/inspect/inspect.go
@@ -1,42 +1,163 @@
 package inspect
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"sort"
+	"strings"
 	"text/tabwriter"
 
+	"github.com/docker/app/render"
 	"github.com/docker/app/types"
+	"github.com/docker/app/types/settings"
+	composetypes "github.com/docker/cli/cli/compose/types"
 )
 
 // Inspect dumps the metadata of an app
-func Inspect(out io.Writer, app *types.App) error {
+func Inspect(out io.Writer, app *types.App, argSettings map[string]string) error {
+	// Render the compose file
+	config, err := render.Render(app, argSettings)
+	if err != nil {
+		return err
+	}
+
+	// Extract all the settings
+	settingsKeys, allSettings, err := extractSettings(app, argSettings)
+	if err != nil {
+		return err
+	}
+
+	var sections []*bytes.Buffer
+
+	// Add Meta data
+	addMetadata(&sections, app)
+
+	// Add Service section
+	addSection(&sections, len(config.Services), func(w io.Writer) {
+		for _, service := range config.Services {
+			fmt.Fprintf(w, "%s\t%d\t%s\t%s\n", service.Name, getReplicas(service), getPorts(service), service.Image)
+		}
+	}, "Service", "Replicas", "Ports", "Image")
+
+	// Add Network section
+	addSection(&sections, len(config.Networks), func(w io.Writer) {
+		for name := range config.Networks {
+			fmt.Fprintln(w, name)
+		}
+	}, "Network")
+
+	// Add Volume section
+	addSection(&sections, len(config.Volumes), func(w io.Writer) {
+		for name := range config.Volumes {
+			fmt.Fprintln(w, name)
+		}
+	}, "Volume")
+
+	// Add Secret section
+	addSection(&sections, len(config.Secrets), func(w io.Writer) {
+		for name := range config.Secrets {
+			fmt.Fprintln(w, name)
+		}
+	}, "Secret")
+
+	// Add Setting section
+	addSection(&sections, len(settingsKeys), func(w io.Writer) {
+		for _, k := range settingsKeys {
+			fmt.Fprintf(w, "%s\t%s\n", k, allSettings[k])
+		}
+	}, "Setting", "Value")
+
+	// Print all sections
+	printSections(out, sections)
+
+	return nil
+}
+
+func addMetadata(sections *[]*bytes.Buffer, app *types.App) {
+	buf := &bytes.Buffer{}
 	meta := app.Metadata()
-	// extract settings
-	settings := app.Settings().Flatten()
+	fmt.Fprintln(buf, meta.Name, meta.Version)
+	if maintainers := meta.Maintainers.String(); maintainers != "" {
+		fmt.Fprintln(buf)
+		fmt.Fprintln(buf, "Maintained by:", maintainers)
+	}
+	if meta.Description != "" {
+		fmt.Fprintln(buf)
+		fmt.Fprintln(buf, meta.Description)
+	}
+	*sections = append(*sections, buf)
+}
+
+func addSection(sections *[]*bytes.Buffer, len int, printer func(io.Writer), headers ...string) {
+	if len == 0 {
+		return
+	}
+	buf := &bytes.Buffer{}
+	w := tabwriter.NewWriter(buf, 0, 0, 1, ' ', 0)
+	printHeaders(w, headers...)
+	printer(w)
+	w.Flush()
+	*sections = append(*sections, buf)
+}
+
+// printSections makes sure there isn't any extra line at the end of the command output
+func printSections(out io.Writer, sections []*bytes.Buffer) {
+	ss := make([]string, len(sections))
+	for i, section := range sections {
+		ss[i] = section.String()
+	}
+	fmt.Fprint(out, strings.Join(ss, "\n"))
+}
+
+func printHeaders(w io.Writer, headers ...string) {
+	fmt.Fprintln(w, strings.Join(headers, "\t"))
+	dashes := make([]string, len(headers))
+	for i, h := range headers {
+		dashes[i] = strings.Repeat("-", len(h))
+	}
+	fmt.Fprintln(w, strings.Join(dashes, "\t"))
+}
+
+func getReplicas(service composetypes.ServiceConfig) int {
+	if service.Deploy.Replicas != nil {
+		return int(*service.Deploy.Replicas)
+	}
+	return 1
+}
+
+func getPorts(service composetypes.ServiceConfig) string {
+	var ports []string
+	for _, port := range service.Ports {
+		if port.Published > 0 {
+			ports = append(ports, fmt.Sprintf("%d", port.Published))
+		}
+	}
+	return strings.Join(ports, ",")
+}
+
+func extractSettings(app *types.App, argSettings map[string]string) ([]string, map[string]string, error) {
+	allSettings, err := mergeAndFlattenSettings(app, argSettings)
+	if err != nil {
+		return nil, nil, err
+	}
 	// sort the keys to get consistent output
 	var settingsKeys []string
-	for k := range settings {
+	for k := range allSettings {
 		settingsKeys = append(settingsKeys, k)
 	}
 	sort.Slice(settingsKeys, func(i, j int) bool { return settingsKeys[i] < settingsKeys[j] })
-	// build maintainers string
-	maintainers := meta.Maintainers.String()
-	fmt.Fprintf(out, "%s %s\n", meta.Name, meta.Version)
-	if maintainers != "" {
-		fmt.Fprintf(out, "Maintained by: %s\n", maintainers)
-		fmt.Fprintln(out, "")
+	return settingsKeys, allSettings, nil
+}
+
+func mergeAndFlattenSettings(app *types.App, argSettings map[string]string) (map[string]string, error) {
+	sArgs, err := settings.FromFlatten(argSettings)
+	if err != nil {
+		return nil, err
 	}
-	if meta.Description != "" {
-		fmt.Fprintf(out, "%s\n", meta.Description)
-		fmt.Fprintln(out, "")
+	s, err := settings.Merge(app.Settings(), sArgs)
+	if err != nil {
+		return nil, err
 	}
-	w := tabwriter.NewWriter(out, 0, 0, 1, ' ', 0)
-	fmt.Fprintln(w, "Setting\tDefault")
-	fmt.Fprintln(w, "-------\t-------")
-	for _, k := range settingsKeys {
-		fmt.Fprintf(w, "%s\t%s\n", k, settings[k])
-	}
-	w.Flush()
-	return nil
+	return s.Flatten(), nil
 }
