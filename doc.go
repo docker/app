cc43dfd057aea079ddcb557934af26481b13b52d
Vincent Demeester <vincent@sbr.pm>
2018-09-06 16:39:39 +0200

    Add package doc for the root package…
    … and an example showing one usage of it as a library.
    The example is executable *and* will be tested when running `go test`.
    
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- doc.go
+++ doc.go
@@ -1,1 +1,9 @@
+// Package app provides experimental utilities to make Compose files more
+// reusable and sharable.
+//
+// The `cmd/docker-app` package generates the `docker-app` binary,
+// see https://github.com/docker/app for more information about it.
+//
+// It can also be used as a library to be integrated in your tools.
+// Usage examples are provided inline with their full documentation.
 package app
