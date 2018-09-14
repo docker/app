4a20de5b474716708487e5aba2792744db325638
Matthieu Nottale <matthieu.nottale@docker.com>
2018-09-14 16:08:22 +0200

    DirNameFromAppName: Accept a trailing '/'.
    Signed-off-by: Matthieu Nottale <matthieu.nottale@docker.com>

--- internal/names.go
+++ internal/names.go
@@ -36,7 +36,7 @@ func AppNameFromDir(dirName string) string {
 // DirNameFromAppName takes an application name and returns the
 // corresponding directory name
 func DirNameFromAppName(appName string) string {
-	if strings.HasSuffix(appName, AppExtension) {
+	if strings.HasSuffix(strings.TrimSuffix(appName, "/"), AppExtension) {
 		return appName
 	}
 	return appName + AppExtension
