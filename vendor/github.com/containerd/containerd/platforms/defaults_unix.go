078835401978dabf9b73e7b0b8400f1a5646583a
Vincent Demeester <vincent@sbr.pm>
2018-12-11 12:17:57 +0100

    github.com/containerd/containerd: v1.1.2 -> v1.2.1
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- vendor/github.com/containerd/containerd/platforms/defaults_unix.go
+++ vendor/github.com/containerd/containerd/platforms/defaults_unix.go
@@ -1,3 +1,5 @@
+// +build !windows
+
 /*
    Copyright The containerd Authors.
 
@@ -16,23 +18,7 @@
 
 package platforms
 
-import (
-	"runtime"
-
-	specs "github.com/opencontainers/image-spec/specs-go/v1"
-)
-
-// DefaultString returns the default string specifier for the platform.
-func DefaultString() string {
-	return Format(DefaultSpec())
-}
-
-// DefaultSpec returns the current platform's default platform specification.
-func DefaultSpec() specs.Platform {
-	return specs.Platform{
-		OS:           runtime.GOOS,
-		Architecture: runtime.GOARCH,
-		// The Variant field will be empty if arch != ARM.
-		Variant: cpuVariant,
-	}
+// Default returns the default matcher for the platform.
+func Default() MatchComparer {
+	return Only(DefaultSpec())
 }
