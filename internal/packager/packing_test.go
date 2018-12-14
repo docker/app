b9c20e336fb97e5a34e00d7d80897de041989f71
Ulysses Souza <ulysses.souza@docker.com>
2018-12-14 10:25:51 +0100

    Revert to docker Untar. TODO use standard untar instead
    Signed-off-by: Ulysses Souza <ulysses.souza@docker.com>

--- internal/packager/packing_test.go
+++ internal/packager/packing_test.go
@@ -2,10 +2,7 @@ package packager
 
 import (
 	"bytes"
-	"os/user"
 	"testing"
-
-	"github.com/docker/docker/pkg/idtools"
 
 	"github.com/docker/app/types"
 	"github.com/docker/docker/pkg/archive"
@@ -20,27 +17,7 @@ func TestPackInvocationImageContext(t *testing.T) {
 	assert.NilError(t, PackInvocationImageContext(app, buf))
 	dir := fs.NewDir(t, t.Name())
 	defer dir.Remove()
-	usr, err := user.Current()
-	assert.NilError(t, err)
-	grp, err := user.LookupGroupId(usr.Gid)
-	assert.NilError(t, err)
-	// In an Unix-like OS, please make sure to have files /etc/sub{uid,gid} filled with:
-	// <your_user_name>:<your_user_id>:1
-	// and
-	// <your_group>:<your_group_id>:1
-	//
-	// An example would be:
-	// /etc/subuid -> myusr:1000:1
-	// and
-	// /etc/subgid -> mygrp:1000:1
-	identityMapping, err := idtools.NewIdentityMapping(usr.Username, grp.Name)
-	assert.NilError(t, err)
-	assert.NilError(t, archive.Untar(buf, dir.Path(),
-		&archive.TarOptions{
-			NoLchown: true,
-			UIDMaps:  identityMapping.UIDs(),
-			GIDMaps:  identityMapping.GIDs(),
-		}))
+	assert.NilError(t, archive.Untar(buf, dir.Path(), nil))
 	expectedDir := fs.NewDir(t, t.Name(),
 		fs.FromDir("testdata/packages"),
 		fs.WithFile("Dockerfile", dockerFile),
