1de712159441f2cf0586f9a5fe94265eec3b6aa9
Simon Ferquel <simon.ferquel@docker.com>
2019-02-11 10:54:16 +0100

    Vendoring for image-map support
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- vendor/github.com/deislabs/duffle/pkg/action/action.go
+++ vendor/github.com/deislabs/duffle/pkg/action/action.go
@@ -1,6 +1,7 @@
 package action
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -39,8 +40,19 @@ func selectInvocationImage(d driver.Driver, c *claim.Claim) (bundle.InvocationIm
 	return bundle.InvocationImage{}, errors.New("driver is not compatible with any of the invocation images in the bundle")
 }
 
+func getImageMap(b *bundle.Bundle) ([]byte, error) {
+	imgs := b.Images
+	if imgs == nil {
+		imgs = make(map[string]bundle.Image)
+	}
+	return json.Marshal(imgs)
+}
+
 func opFromClaim(action string, c *claim.Claim, ii bundle.InvocationImage, creds credentials.Set, w io.Writer) (*driver.Operation, error) {
 	env, files, err := creds.Expand(c.Bundle)
+	if err != nil {
+		return nil, err
+	}
 
 	// Quick verification that no params were passed that are not actual legit params.
 	for key := range c.Parameters {
@@ -68,6 +80,12 @@ func opFromClaim(action string, c *claim.Claim, ii bundle.InvocationImage, creds
 		}
 	}
 
+	imgMap, err := getImageMap(c.Bundle)
+	if err != nil {
+		return nil, fmt.Errorf("unable to generate image map: %s", err)
+	}
+	files["/cnab/app/image-map.json"] = string(imgMap)
+
 	env["CNAB_INSTALLATION_NAME"] = c.Name
 	env["CNAB_ACTION"] = action
 	env["CNAB_BUNDLE_NAME"] = c.Bundle.Name
@@ -83,5 +101,5 @@ func opFromClaim(action string, c *claim.Claim, ii bundle.InvocationImage, creds
 		Environment:  env,
 		Files:        files,
 		Out:          w,
-	}, err
+	}, nil
 }
