cb55dae4adedd6c8791222fbdcbea7d4fea88da2
Silvin Lubecki <silvin.lubecki@docker.com>
2019-03-19 18:07:22 +0100

    Simplify and fix CNAB bundle generation test, using a golden file instead.
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- internal/packager/cnab_test.go
+++ internal/packager/cnab_test.go
@@ -1,11 +1,11 @@
 package packager
 
 import (
+	"encoding/json"
 	"testing"
 
-	"github.com/docker/app/internal"
+	"gotest.tools/golden"
 
-	"github.com/deislabs/duffle/pkg/bundle"
 	"github.com/docker/app/types"
 	"gotest.tools/assert"
 )
@@ -15,99 +15,7 @@ func TestToCNAB(t *testing.T) {
 	assert.NilError(t, err)
 	actual, err := ToCNAB(app, "test-image")
 	assert.NilError(t, err)
-	expected := &bundle.Bundle{
-		Description: "hello",
-		Name:        "packing",
-		Maintainers: []bundle.Maintainer{
-			{Name: "bearclaw", Email: "bearclaw@bearclaw.com"},
-			{Name: "bob", Email: "bob@bob.com"},
-		},
-		Version: "0.1.0",
-		InvocationImages: []bundle.InvocationImage{
-			{
-				BaseImage: bundle.BaseImage{
-					Image:     "test-image",
-					ImageType: "docker",
-				},
-			},
-		},
-		Credentials: map[string]bundle.Location{
-			"docker.context": {
-				Path: "/cnab/app/context.dockercontext",
-			},
-		},
-		Parameters: map[string]bundle.ParameterDefinition{
-			"docker.orchestrator": {
-				DataType: "string",
-				AllowedValues: []interface{}{
-					"",
-					"swarm",
-					"kubernetes",
-				},
-				Destination: &bundle.Location{
-					EnvironmentVariable: "DOCKER_STACK_ORCHESTRATOR",
-				},
-				Metadata: bundle.ParameterMetadata{
-					Description: "Orchestrator on which to deploy",
-				},
-				DefaultValue: "",
-			},
-			"docker.kubernetes-namespace": {
-				DataType: "string",
-				Destination: &bundle.Location{
-					EnvironmentVariable: "DOCKER_KUBERNETES_NAMESPACE",
-				},
-				Metadata: bundle.ParameterMetadata{
-					Description: "Namespace in which to deploy",
-				},
-				DefaultValue: "",
-			},
-			"watcher.cmd": {
-				DataType: "string",
-				Destination: &bundle.Location{
-					EnvironmentVariable: "docker_param1",
-				},
-				DefaultValue: "foo",
-			},
-		},
-		Actions: map[string]bundle.Action{
-			internal.Namespace + "inspect": {
-				Modifies: false,
-			},
-			internal.Namespace + "status": {
-				Modifies: false,
-			},
-		},
-		Images: map[string]bundle.Image{
-			"front": {
-				Description: "nginx",
-				BaseImage: bundle.BaseImage{
-					Image:     "nginx",
-					ImageType: "docker",
-				},
-			},
-			"debug": {
-				Description: "busybox:latest",
-				BaseImage: bundle.BaseImage{
-					Image:     "busybox:latest",
-					ImageType: "docker",
-				},
-			},
-			"monitor": {
-				Description: "busybox:latest",
-				BaseImage: bundle.BaseImage{
-					Image:     "busybox:latest",
-					ImageType: "docker",
-				},
-			},
-			"app-watcher": {
-				Description: "watcher",
-				BaseImage: bundle.BaseImage{
-					Image:     "watcher",
-					ImageType: "docker",
-				},
-			},
-		},
-	}
-	assert.DeepEqual(t, actual, expected)
+	actualJSON, err := json.MarshalIndent(actual, "", "  ")
+	assert.NilError(t, err)
+	golden.Assert(t, string(actualJSON), "bundle-json.golden")
 }
