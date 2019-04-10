d69c4951ee514970b3accff8bdb2dc1ed8ad857b
Simon Ferquel <simon.ferquel@docker.com>
2019-04-10 17:36:06 +0200

    consume auto-parameters
    Signed-off-by: Simon Ferquel <simon.ferquel@docker.com>

--- cmd/cnab-run/inspect.go
+++ cmd/cnab-run/inspect.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"os"
 
 	appinspect "github.com/docker/app/internal/inspect"
 	"github.com/docker/app/internal/packager"
+	"github.com/docker/app/types"
+	"github.com/pkg/errors"
 )
 
 func inspectAction(instanceName string) error {
-	app, err := packager.Extract("")
+	overrides, err := parseOverrides()
+	if err != nil {
+		return errors.Wrap(err, "unable to parse auto-parameter values")
+	}
+	app, err := packager.Extract("", types.WithComposes(bytes.NewReader(overrides)))
 	// todo: merge additional compose file
 	if err != nil {
 		return err
--- cmd/cnab-run/install.go
+++ cmd/cnab-run/install.go
@@ -1,27 +1,33 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"strconv"
+	"strings"
 
 	"github.com/deislabs/duffle/pkg/bundle"
 	"github.com/docker/app/internal"
 	"github.com/docker/app/internal/packager"
 	"github.com/docker/app/render"
+	"github.com/docker/app/types"
 	"github.com/docker/cli/cli/command"
 	"github.com/docker/cli/cli/command/stack"
 	"github.com/docker/cli/cli/command/stack/options"
 	"github.com/docker/cli/cli/command/stack/swarm"
 	"github.com/pkg/errors"
 	"github.com/spf13/pflag"
+	yaml "gopkg.in/yaml.v2"
 )
 
 const (
 	// imageMapFilePath is the path where the CNAB runtime will put the actual
 	// service to image mapping to use
 	imageMapFilePath = "/cnab/app/image-map.json"
+	overridesDir     = "/cnab/app/overrides"
 )
 
 func installAction(instanceName string) error {
@@ -29,7 +35,11 @@ func installAction(instanceName string) error {
 	if err != nil {
 		return errors.Wrap(err, "unable to restore docker context")
 	}
-	app, err := packager.Extract("")
+	overrides, err := parseOverrides()
+	if err != nil {
+		return errors.Wrap(err, "unable to parse auto-parameter values")
+	}
+	app, err := packager.Extract("", types.WithComposes(bytes.NewReader(overrides)))
 	// todo: merge additional compose file
 	if err != nil {
 		return err
@@ -83,4 +93,42 @@ func getBundleImageMap() (map[string]bundle.Image, error) {
 		return nil, err
 	}
 	return result, nil
+}
+
+func parseOverrides() ([]byte, error) {
+	root := make(map[string]interface{})
+	if err := filepath.Walk(overridesDir, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !fi.IsDir() {
+			bytes, err := ioutil.ReadFile(path)
+			if err != nil {
+				return err
+			}
+			if len(bytes) > 0 {
+				rel, err := filepath.Rel(overridesDir, path)
+				if err != nil {
+					return err
+				}
+				splitPath := strings.Split(rel, "/")
+				setValue(root, splitPath, string(bytes))
+			}
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return yaml.Marshal(root)
+}
+
+func setValue(root map[string]interface{}, path []string, value string) {
+	key, sub := path[0], path[1:]
+	if len(sub) == 0 {
+		root[key] = value
+		return
+	}
+	subMap := make(map[string]interface{})
+	setValue(subMap, sub, value)
+	root[key] = subMap
 }
