472296922536b5817e7c764b37569d90af41950e
Silvin Lubecki <silvin.lubecki@docker.com>
2019-04-24 15:27:27 +0200

    Add --tag flag to the bundle command to persist the bundle in the local bundle store. Installation or any other command will be able to use this reference. Invocation image is using the same name, appending "-invoc" to the tag.
    Signed-off-by: Silvin Lubecki <silvin.lubecki@docker.com>

--- internal/commands/bundle.go
+++ internal/commands/bundle.go
@@ -10,10 +10,12 @@ import (
 	"github.com/deislabs/cnab-go/bundle"
 	"github.com/docker/app/internal"
 	"github.com/docker/app/internal/packager"
+	"github.com/docker/app/internal/store"
 	"github.com/docker/app/types"
 	"github.com/docker/app/types/metadata"
 	"github.com/docker/cli/cli"
 	"github.com/docker/cli/cli/command"
+	"github.com/docker/cli/cli/config"
 	"github.com/docker/distribution/reference"
 	dockertypes "github.com/docker/docker/api/types"
 	"github.com/docker/docker/pkg/jsonmessage"
@@ -23,6 +25,7 @@ import (
 
 type bundleOptions struct {
 	out string
+	tag string
 }
 
 func bundleCmd(dockerCli command.Cli) *cobra.Command {
@@ -38,17 +41,27 @@ func bundleCmd(dockerCli command.Cli) *cobra.Command {
 	}
 
 	cmd.Flags().StringVarP(&opts.out, "output", "o", "bundle.json", "Output file (- for stdout)")
+	cmd.Flags().StringVarP(&opts.tag, "tag", "t", "", "Name and optionally a tag in the 'name:tag' format")
 	return cmd
 }
 
 func runBundle(dockerCli command.Cli, appName string, opts bundleOptions) error {
-	bundle, err := makeBundle(dockerCli, appName)
+	ref, err := getNamedTagged(opts.tag)
+	if err != nil {
+		return err
+	}
+
+	bundle, err := makeBundle(dockerCli, appName, ref)
 	if err != nil {
 		return err
 	}
 	if bundle == nil || len(bundle.InvocationImages) == 0 {
 		return fmt.Errorf("failed to create bundle %q", appName)
 	}
+	if err := persistInBundleStore(ref, bundle); err != nil {
+		return err
+	}
+
 	fmt.Fprintf(dockerCli.Out(), "Invocation image %q successfully built\n", bundle.InvocationImages[0].Image)
 	bundleBytes, err := json.MarshalIndent(bundle, "", "\t")
 	if err != nil {
@@ -61,18 +74,18 @@ func runBundle(dockerCli command.Cli, appName string, opts bundleOptions) error 
 	return ioutil.WriteFile(opts.out, bundleBytes, 0644)
 }
 
-func makeBundle(dockerCli command.Cli, appName string) (*bundle.Bundle, error) {
+func makeBundle(dockerCli command.Cli, appName string, refOverride reference.NamedTagged) (*bundle.Bundle, error) {
 	app, err := packager.Extract(appName)
 	if err != nil {
 		return nil, err
 	}
 	defer app.Cleanup()
-	return makeBundleFromApp(dockerCli, app)
+	return makeBundleFromApp(dockerCli, app, refOverride)
 }
 
-func makeBundleFromApp(dockerCli command.Cli, app *types.App) (*bundle.Bundle, error) {
+func makeBundleFromApp(dockerCli command.Cli, app *types.App, refOverride reference.NamedTagged) (*bundle.Bundle, error) {
 	meta := app.Metadata()
-	invocationImageName, err := makeInvocationImageName(meta)
+	invocationImageName, err := makeInvocationImageName(meta, refOverride)
 	if err != nil {
 		return nil, err
 	}
@@ -102,14 +115,47 @@ func makeBundleFromApp(dockerCli command.Cli, app *types.App) (*bundle.Bundle, e
 	return packager.ToCNAB(app, invocationImageName)
 }
 
-func makeInvocationImageName(meta metadata.AppMetadata) (string, error) {
-	return makeCNABImageName(meta, "-invoc")
+func makeInvocationImageName(meta metadata.AppMetadata, refOverride reference.NamedTagged) (string, error) {
+	if refOverride != nil {
+		return makeCNABImageName(reference.FamiliarName(refOverride), refOverride.Tag(), "-invoc")
+	}
+	return makeCNABImageName(meta.Name, meta.Version, "-invoc")
 }
 
-func makeCNABImageName(meta metadata.AppMetadata, suffix string) (string, error) {
-	name := fmt.Sprintf("%s:%s%s", meta.Name, meta.Version, suffix)
+func makeCNABImageName(appName, appVersion, suffix string) (string, error) {
+	name := fmt.Sprintf("%s:%s%s", appName, appVersion, suffix)
 	if _, err := reference.ParseNormalizedNamed(name); err != nil {
 		return "", errors.Wrapf(err, "image name %q is invalid, please check name and version fields", name)
 	}
 	return name, nil
+}
+
+func persistInBundleStore(ref reference.Named, bndle *bundle.Bundle) error {
+	if ref == nil {
+		return nil
+	}
+	appstore, err := store.NewApplicationStore(config.Dir())
+	if err != nil {
+		return err
+	}
+	bundleStore, err := appstore.BundleStore()
+	if err != nil {
+		return err
+	}
+	return bundleStore.Store(ref, bndle)
+}
+
+func getNamedTagged(tag string) (reference.NamedTagged, error) {
+	if tag == "" {
+		return nil, nil
+	}
+	namedRef, err := reference.ParseNormalizedNamed(tag)
+	if err != nil {
+		return nil, err
+	}
+	ref, ok := reference.TagNameOnly(namedRef).(reference.NamedTagged)
+	if !ok {
+		return nil, fmt.Errorf("tag %q must be name with a tag in the 'name:tag' format", tag)
+	}
+	return ref, nil
 }
