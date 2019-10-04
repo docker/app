32afd02816abe236f998ad054e0872f229835b00
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-10-04 14:18:17 +0200

    Remove --output option
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- e2e/build_test.go
+++ e2e/build_test.go
@@ -4,12 +4,11 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"path"
+	"strings"
 	"testing"
 
 	"github.com/deislabs/cnab-go/bundle"
 	"gotest.tools/assert"
-	"gotest.tools/fs"
-
 	"gotest.tools/icmd"
 )
 
@@ -18,12 +17,20 @@ func TestBuild(t *testing.T) {
 		cmd := info.configuredCmd
 
 		testDir := path.Join("testdata", "build")
-		dir := fs.NewDir(t, "test-name")
-		defer dir.Remove()
-		f := dir.Join("bundle.json")
-		cmd.Command = dockerCli.Command("app", "build", path.Join(testDir, "single"), "a-simple-tag", "--output", f)
+		cmd.Command = dockerCli.Command("app", "build", path.Join(testDir, "single"), "single:1.0.0")
 		icmd.RunCmd(cmd).Assert(t, icmd.Success)
 
+		var cfg string
+		for _, s := range cmd.Env {
+			if strings.HasPrefix(s, "DOCKER_CONFIG=") {
+				cfg = s[14:]
+			}
+		}
+		if cfg == "" {
+			t.Fatalf("Failed to retrieve docker config folder")
+		}
+
+		f := path.Join(cfg, "app", "bundles", "docker.io", "library", "single", "_tags", "1.0.0.json")
 		data, err := ioutil.ReadFile(f)
 		assert.NilError(t, err)
 		var bndl bundle.Bundle
