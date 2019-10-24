f9f8aac0dedca1968707fc2d6cf5e5e2d5362a78
Nicolas De Loof <nicolas.deloof@gmail.com>
2019-10-24 11:01:51 +0200

    Validate parameters against the actual compose file
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- internal/commands/validate.go
+++ internal/commands/validate.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/docker/app/internal/compose"
 	"github.com/docker/app/internal/packager"
 	"github.com/docker/app/render"
 	"github.com/docker/app/types"
 	"github.com/docker/cli/cli"
 	cliopts "github.com/docker/cli/opts"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
@@ -23,22 +25,37 @@ func validateCmd() *cobra.Command {
 		Short: "Checks the rendered application is syntactically correct",
 		Args:  cli.RequiresMaxArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			app, err := packager.Extract(firstOrEmpty(args),
-				types.WithParametersFiles(opts.parametersFiles...),
-			)
-			if err != nil {
-				return err
-			}
-			defer app.Cleanup()
-			argParameters := cliopts.ConvertKVStringsToMap(opts.overrides)
-			_, err = render.Render(app, argParameters, nil)
-			if err != nil {
-				return err
-			}
-			fmt.Fprintf(os.Stdout, "Validated %q\n", app.Path)
-			return nil
+			return runValidate(args, opts)
 		},
 	}
 	opts.parametersOptions.addFlags(cmd.Flags())
 	return cmd
+}
+
+func runValidate(args []string, opts validateOptions) error {
+	app, err := packager.Extract(firstOrEmpty(args),
+		types.WithParametersFiles(opts.parametersFiles...),
+	)
+	if err != nil {
+		return err
+	}
+	defer app.Cleanup()
+	argParameters := cliopts.ConvertKVStringsToMap(opts.overrides)
+	_, err = render.Render(app, argParameters, nil)
+	if err != nil {
+		return err
+	}
+
+	vars, err := compose.ExtractVariables(app.Composes()[0], compose.ExtrapolationPattern)
+	if err != nil {
+		return errors.Wrap(err, "failed to parse compose file")
+	}
+	for k := range app.Parameters().Flatten() {
+		if _, ok := vars[k]; !ok {
+			return fmt.Errorf("%s is declared as parameter but not used by the compose file", k)
+		}
+	}
+
+	fmt.Fprintf(os.Stdout, "Validated %q\n", app.Path)
+	return nil
 }
